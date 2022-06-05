a991e8054defae2e84b2df85ed757adaa6af01cd
harshitvijay <harshitvijay007@gmail.com>
2022-06-05 15:11:21 +0530

    changes port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 const app = require("./api/server");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, console.log(`Server Running at port ${port}`));
